0c5318472beecbf2096d94e2480de41f9d79a4b3
sarita-sumbria <saritasumbria@gmail.com>
2020-02-13 16:53:32 +0000

    Created test for likes

--- test_helper.go
+++ test_helper.go
@@ -44,3 +44,21 @@ func populateDB() {
   db.Exec(sqlStatement_2)
   db.Exec(sqlStatement_3)
 }
+
+func populateDBLikes() {
+  psqlInfo := fmt.Sprintf("host=%s port=%d  "+
+    " dbname=%s sslmode=disable",
+    host, port, dbname)
+  db, err := sql.Open("postgres", psqlInfo)
+  if err != nil {
+    panic(err)
+  }
+  defer db.Close()
+
+  sqlStatement_1 := `INSERT INTO users (user_id, first_name, last_name, email, password) VALUES (1, 'name_test', 'surname_test', 'test@test.com', '123')`
+  sqlStatement_2 := `INSERT INTO posts (post_id, created_by, content) VALUES (10, 1, 'Test post')`
+
+  db.Exec(sqlStatement_1)
+  db.Exec(sqlStatement_2)
+
+}
